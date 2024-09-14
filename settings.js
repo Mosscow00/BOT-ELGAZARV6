a282fc3a27f1d6751bfc912305205fc5b0dbf8b1
Mosscow00 <141783259+Mosscow00@users.noreply.github.com>
2024-09-14 13:04:38 +0300

    Update settings.js

--- settings.js
+++ settings.js
@@ -15,23 +15,23 @@ global.doc5 = 'application/pdf'
 global.doc6 = 'application/vnd.android.package-archive'
 
 //owmner v card
-global.owner = ['201098906252'] //ur owner number
-global.ownername = "ᗴᒪᘜᗩᘔᗩᖇ ᗩᒪᗯᗩᘔᗴᖇ❤️" //ur owner name
-global.ytname = "YT: ELGAZAR" //ur yt chanel name
-global.socialm = "GitHub: ELGAZAR" //ur github or insta name
+global.owner = ['201210307869'] //ur owner number
+global.ownername = "حرب اليكس" //ur owner name
+global.ytname = "YT: itsmoscow" //ur yt chanel name
+global.socialm = "GitHub: imosscow" //ur github or insta name
 global.location = "Africa, Egypt, Cairo" //ur location
 
 //bot bomdy 
-global.ownernomer = "201098906252" //ur number
-global.premium = ['201098906252'] //ur premium number
-global.botname = '❤️ᗴᒪᘜᗩᘔᗩᖇ ᗷOT❤️' //ur bot name
-global.linkz = "https://youtube.com/@ABDALLAH_MOHAMED" //your theme url which will be displayed on whatsapp
+global.ownernomer = "201210307869" //ur number
+global.premium = ['201210307869'] //ur premium number
+global.botname = 'حرب اليكس' //ur bot name
+global.linkz = "https://youtube.com/@itsmosscow" //your theme url which will be displayed on whatsapp
 global.websitex = "https://telegra.ph/file/ec763cc3e7c88d0e67682.jpg" //ur website to be displayed
-global.botscript = 'https://youtube.com/@ABDALLAH_MOHAMED' //script link
+global.botscript = 'https://youtube.com/@itsmosscow' //script link
 global.themeemoji = "♥" //ur theme emoji
-global.packname = "⍣الجزار الوزير يروحي⍣" //ur sticker watermark packname
-global.author = "⍣01098906252⍣" //ur sticker watermark author
-global.wm = "❤️ᗴᒪᘜᗩᘔᗩᖇ ᗷOT❤️." //ur watermark
+global.packname = "حرب" //ur sticker watermark packname
+global.author = "201210307869" //ur sticker watermark author
+global.wm = "حرب اليكس" //ur watermark
 
 // Other
 global.sessionName = 'session'
@@ -40,16 +40,16 @@ global.sp = ''
 
 //messages
 global.mess = {
-    success: 'تم يروحي✨',
-    admin: '『 الامر دا لادمن بس! 』',
-    botAdmin: '『 ارفع البوت ادمن الاول 』',
-    premime: '『 ميزات خاصة متميزة إذا كنت ترغب في تسجيل نوع الإيجار 』',
-    owner: '『 الميزه دي للمطور بس! 』',
-    group: '『 الميزه دي للجروبات بس! 』',
-    private: '『 الميزه دي في الخاص بس! 』',
-    bot: '『 الميزه دي للبوت بس! 』',
-    wait: '『 استني شويه... 』',
-    linkm: '『 فين الرابط? 』',
+    success: 'تم تنفيذ الاوامر',
+    admin: 'يجب الحصول على ادمن لتفنقيذ الامر !!!',
+    botAdmin: 'لتنفيذ الاوامر يرجى رفع البوت ادمن !!!',
+    premime: 'مميزات خاصة',
+    owner: 'المميزات هذه لمالك البوت',
+    group: 'المميزات هذه للجروبات فقط',
+    private: 'المميزات هذه للبرايڤيت',
+    bot: 'المميزات هذه للبوت فقط',
+    wait: '. . . ويت',
+    linkm: '',
     endLimit: 'انتهت صلاحية الحد اليومي الخاص بك ، وسيتم إعادة تعيين الحد كل 12 ساعة',
     nsfw: 'لم يتم تنشيط ميزة nsfw ، يرجى الاتصال بالمسؤول للتفعيل',
 }
